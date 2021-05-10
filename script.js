d149a4945c09d029d2284cdce7998d1774fbbeda
Yura <nastasya.ustinskaya@bk.ru>
2021-05-10 22:03:18 +0600

    аче

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 "use strict";
-let userName = prompt("Введите имя персанажа");
+let newHeight = document.documentElement.clientHeight;
+let body = document.querySelector("body");
+body.style.cssText = `min-height: ${newHeight}px`;
+let rightMenu = document.querySelector("#rightMenu");
+let newRightMenuHeight = newHeight - 85; 
+rightMenu.style.cssText = `height: ${newRightMenuHeight}px`;
