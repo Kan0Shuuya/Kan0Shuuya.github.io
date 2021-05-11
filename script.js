dea61640908ee0bdfec5e730de75a2ad693814bc
Yura <nastasya.ustinskaya@bk.ru>
2021-05-11 10:08:26 +0600

    короче, да

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ body.style.cssText = `min-height: ${newHeight}px`;
 let rightMenu = document.querySelector("#rightMenu");
 let newRightMenuHeight = newHeight - 85; 
 rightMenu.style.cssText = `height: ${newRightMenuHeight}px`;
+
+let user = {
+    img: "",
+};
+
+reg.onclick = function (){
+    user.img = prompt("Введите ссылку на картинку");
+    document.querySelector("#avatar").src = user.img;
+};
